2c7ef64760d51a5ff3b130e96e4f87047cb4ade1
Chloe Guthrie <chloeruthguthrie@gmail.com>
2023-10-15 17:16:32 -0700

    initial commit

---
+++ monster_parser.js
@@ -0,0 +1,45 @@
+import {readFile, writeFile} from 'fs/promises';
+
+const monsterFile = await readMonsters();
+const monsterData = await createMonsterFile();
+writeToFile(monsterData);
+
+async function createMonsterFile() {
+    // DrakSlime  45 13 16 10 13  8 20 16 BeDragon  FireAir   SuckAir Slime
+    return monsterFile.toString().split('\n').map( (monster) => {
+        const params = monster.split(/\W+/);
+        return {
+            name: params[0],
+            levelLimit: params[1],
+            growth: params[2],
+            hp: params[3],
+            mp: params[4],
+            attack: params[5],
+            defense: params[6],
+            agility: params[7],
+            intelligence: params[8],
+            skill1: params[9],
+            skill2: params[10],
+            skill3: params[11],
+            family: params[12]
+        };
+    });
+}
+
+async function readMonsters() {
+    try {
+        const monsters = await readFile('monster_data.txt', 'utf-8');
+        return monsters;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function writeToFile(data) {
+    try {
+        await writeFile('monster_json.txt', JSON.stringify(data));
+        console.log("file written successfully!");
+    } catch (error) {
+        console.log(error);
+    }
+}
